b798c2c5a3e8cfdce896341d5b3eaaa20183fc02
daniel.mlaszlo@yahoo.com <daniel.mlaszlo@yahoo.com>
2023-07-11 19:54:27 +0200

    changes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,14 @@
-import { Controller, Get, Post, Put, Delete, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
 import { ReportType, data } from 'src/data';
+import { v4 as uuid } from 'uuid';
 
 @Controller('api/v1/report/:type')
 export class AppController {
@@ -10,8 +19,21 @@ export class AppController {
     return data.report.filter((report) => report.type === reportType);
   }
   @Post('')
-  createReport() {
-    return 'Created';
+  createReport(
+    @Body() { amount, source }: { amount: number; source: string },
+    @Param('type') type: string,
+  ) {
+    const newReport = {
+      id: uuid(),
+      source,
+      amount,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: type === 'income' ? ReportType.INCOME : ReportType.EXPENSE,
+    };
+
+    data.report.push(newReport);
+    return newReport;
   }
   @Get(':id')
   getReportById(@Param('type') type: string, @Param('id') id: string) {
@@ -23,8 +45,29 @@ export class AppController {
       .find((report) => report.id === id);
   }
   @Put(':id')
-  updateReport() {
-    return 'Updated';
+  updateReport(
+    @Param('type') type: string,
+    @Param('id') id: string,
+    @Body() body: { amount: number; source: string },
+  ) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+
+    const reportToUpdate = data.report
+      .filter((report) => report.type === reportType)
+      .find((report) => report.id === id);
+
+    if (!reportToUpdate) return;
+
+    const reportIndex = data.report.findIndex(
+      (report) => report.id === reportToUpdate.id,
+    );
+
+    data.report[reportIndex] = {
+      ...data.report[reportIndex],
+      ...body,
+    };
+    return data.report[reportIndex];
   }
   @Delete(':id')
   deleteReport() {
